bb8459dbccae299ce495d075c627d7b83b1dad22
Lepiloff <lepiloff82@gmail.com>
2025-10-09 19:08:27 +0200

    add multilang to admin

--- apps/strains/templatetags/translate.py
+++ apps/strains/templatetags/translate.py
@@ -1,8 +1,18 @@
 from django import template
+from django.utils import translation
 
 register = template.Library()
 
 
 @register.filter
-def translate(value, translations_dict):
-    return translations_dict.get(value, value)
+def translate(value, translations_dict=None):
+    """
+    Deprecated: This filter is kept for backward compatibility.
+    With django-modeltranslation, translations are now stored in the database.
+
+    Simply returns the value as-is since modeltranslation handles language switching automatically.
+    When a proper language switcher is implemented, Django's translation.get_language() will
+    determine which field (_en or _es) to return.
+    """
+    # Return value as-is - modeltranslation handles the language switching
+    return value
