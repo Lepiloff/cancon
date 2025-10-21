25f4acdaf90da840838bb4fcd28fc6cd3d7ad0ef
Lepiloff <lepiloff82@gmail.com>
2025-10-21 19:04:23 +0200

    fix sitemap, made chat optional

--- canna/urls.py
+++ canna/urls.py
@@ -40,6 +40,10 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('i18n/', include('django.conf.urls.i18n')),  # Language switcher endpoint
     path('robots.txt', robots_txt, name='robots_txt'),  # SEO: robots.txt
+    path('sitemap.xml', sitemap, {
+        'sitemaps': sitemaps,
+        'template_name': 'sitemap.xml',  # Custom template with hreflang support
+    }, name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 # Multi-language URLs with i18n_patterns
@@ -49,10 +53,6 @@ urlpatterns += i18n_patterns(
     path('', include('apps.strains.urls')),
     path('store/', include('apps.store.urls')),
     path('api/chat/', include('apps.chat_bot.urls')),
-    path('sitemap.xml', sitemap, {
-        'sitemaps': sitemaps,
-        'template_name': 'sitemap.xml',  # Custom template with hreflang support
-    }, name='django.contrib.sitemaps.views.sitemap'),
     prefix_default_language=False,  # Spanish WITHOUT prefix - CRITICAL for SEO!
 )
 
