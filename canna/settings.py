42026201b5679c239859fc2d140145967e868207
Lepiloff <lepiloff82@gmail.com>
2026-02-15 20:01:46 +0100

    fix tinymce

--- canna/settings.py
+++ canna/settings.py
@@ -215,13 +215,12 @@ TINYMCE_DEFAULT_CONFIG = {
     "height": "320px",
     "width": "960px",
     "menubar": "file edit view insert format tools table help",
-    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
-    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
-    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
-    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
-    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
-    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
-    "a11ycheck ltr rtl | showcomments addcomment code",
+    "plugins": "advlist autolink lists link image charmap preview anchor searchreplace visualblocks code "
+    "fullscreen insertdatetime table help wordcount",
+    "toolbar": "undo redo | bold italic underline strikethrough | blocks fontsize | alignleft "
+    "aligncenter alignright alignjustify | outdent indent | numlist bullist | forecolor "
+    "backcolor removeformat | pagebreak | charmap emoticons | "
+    "fullscreen preview | insertfile image link anchor codesample | code",
     "custom_undo_redo_levels": 10,
 }
 
